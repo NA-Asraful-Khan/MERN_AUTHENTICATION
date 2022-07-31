6f18f21a300665c2dda0d21bdd723c3258cc5ac6
NA-Asraful-Khan <soyonshikhor@gmail.com>
2022-08-01 00:25:39 +0600

    Mongo DB connected

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 require('dotenv').config({path: "./config.env"})
 const express = require('express')
+const connectDB = require('./config/db');
+
+//Connect DB
+
+
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 
 
+
 app.get('/', (req, res) => {
     res.send("Success")
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`My Web site is running on port: ${port}`)
+})
+
+process.on("unhandledRejection",(err,pronise) => {
+    console.log(`Logged Error: ${err}`);
+    server.close(()=> process.exit(1));
 })
