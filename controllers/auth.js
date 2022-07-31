c72d699774d161a26f6396dd3d5a611adbdddf69
NA-Asraful-Khan <soyonshikhor@gmail.com>
2022-08-01 03:27:34 +0600

    Sign up and login done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 //id: admin
 //pass: GpApk9cSzykONOcI
@@ -7,7 +7,7 @@ exports.register = async (req,res,next)=>{
     const {username,email,password} = req.body;
     try{
         const user = await User.create({
-            username,email, password 
+            username,email, password
         });
         res.status(201).json({
             success:true,
@@ -21,8 +21,31 @@ exports.register = async (req,res,next)=>{
     }
 };
 
-exports.login = (req,res,next)=>{
-    res.send("Login Route");
+exports.login = async (req,res,next)=>{
+    const {email,password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({success: false, error:"Please provide email and password"});
+    }
+
+    try{
+        const user = await User.findOne({email}).select("+password");
+        if(!user){
+            return res.status(404).json({ success: false, error: "No account found"});
+        }
+
+        const isMatch = await user.matchPassword(password);
+
+        if(!isMatch){
+            return res.status(404).json({success: false, error:"Wrong Password"});
+        }
+
+        res.status(200).json({
+            success:true,
+            token:"treasdf4565sadf"
+        })
+    }catch(error){
+        res.status(500).json({success:false, error: error.message});
+    }
 };
 
 exports.forgotPassword = (req,res,next)=>{
